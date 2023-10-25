19b0ea959451bbf7f00caab4c86465598936d2fc
sahumajurado <2113110059@untels.edu.pe>
2023-10-25 09:38:21 -0500

    Listo por Sahuma

---
+++ aplicacion.py
@@ -0,0 +1,11 @@
+def menu():
+    print("Menú Opciones")
+    print("1. Registrar")
+    print("2. Eliminar")
+    print("3. Editar")
+    print("4. Listar")
+    print("5. Salir")
+
+while True:
+    menu()
+    opcion = int(input("Seleccionar una opción: "))
