4066aabc73c6ea46d302a7c07875ca390da85ba7
Brian Inca <brianincah10@gmail.com>
2023-10-25 10:33:06 -0500

    Listo por Inca

--- aplicacion.py
+++ aplicacion.py
@@ -1,18 +1,19 @@
 import sqlite3
 
-#Conexion a base de datos
-conexion = sqlite3.connect("AsuncionPomasonco_almacen.db")
-cursor = conexion.cursor()
-#Crear tabla producto
-tabla_producto = """CREATE TABLE producto(
-                idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
-                codigo TEXT UNIQUE,
-                nombre TEXT UNIQUE,
-                precio REAL
-                )"""
-cursor.execute(tabla_producto)
-cursor.close()
-conexion.close()
+# # Conexion a base de datos
+# conexion = sqlite3.connect("AsuncionPomasonco_almacen.db")
+# cursor = conexion.cursor()
+# # Crear tabla producto
+# tabla_producto = """CREATE TABLE producto(
+#                 idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+#                 codigo TEXT UNIQUE,
+#                 nombre TEXT UNIQUE,
+#                 precio REAL
+#                 )"""
+# cursor.execute(tabla_producto)
+# cursor.close()
+# conexion.close()
+
 
 def menu():
     print("Menú Opciones")
@@ -22,6 +23,50 @@ def menu():
     print("4. Listar")
     print("5. Salir")
 
-while True:
-    menu()
-    opcion = int(input("Seleccionar una opción: "))
+
+def listar_productos():
+    sql = "SELECT * FROM producto"
+    with sqlite3.connect("AsuncionPomasonco_almacen.db") as con:
+        cursor = con.cursor()
+        cursor.execute(sql)
+        productos = cursor.fetchall()
+        print("ID\tCódigo\tNombre\tPrecio")
+        print("-"*50)
+        for producto in productos:
+            print(producto[0], producto[1], producto[2], producto[3], sep="\t")
+
+
+def registrar_producto(codigo, nombre, precio):
+    sql = "INSERT INTO producto(codigo, nombre, precio) VALUES(?,?,?)"
+    with sqlite3.connect("AsuncionPomasonco_almacen.db") as con:
+        cursor = con.cursor()
+        cursor.execute(sql, (codigo, nombre, precio))
+        con.commit()
+        print("Producto registrado con éxito")
+
+# Registrar 10 productos
+def registrar_10_productos():
+    productos = [
+        ("P001", "Café Tunki", 25.00),
+        ("P002", "Chocolates La Iberica", 15.50),
+        ("P003", "Pisco Portón", 80.00),
+        ("P004", "Aceitunas Botija", 12.00),
+        ("P005", "Chicha Morada", 5.00),
+        ("P006", "Cerveza Cusqueña", 8.00),
+        ("P007", "Inka Kola", 3.50),
+        ("P008", "Papa Seca", 6.00),
+        ("P009", "Leche Gloria", 4.50),
+        ("P010", "Cuy Asado", 30.00)
+    ]
+    sql = "INSERT INTO producto (codigo, nombre, precio) VALUES (?,?,?)"
+    with sqlite3.connect("AsuncionPomasonco_almacen.db") as con:
+        cursor = con.cursor()
+        cursor.executemany(sql, productos)
+        print("10 productos insertados con éxito")
+
+
+# while True:
+#     menu()
+#     opcion = int(input("Seleccionar una opción: "))
+
+registrar_10_productos()
